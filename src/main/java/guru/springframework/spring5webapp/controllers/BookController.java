87bf09a2260a96342297440ac4a3e471914d281b
zhikin <zhikin.mok@gmail.com>
2019-01-20 23:24:20 +0100

    thymleaf example with books (GUI) added

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
-    
+
     private BookRepository bookRepository;
 
     public BookController(BookRepository bookRepository) {
